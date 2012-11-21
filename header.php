c060f55b8f9583247bbb3bb89f5e7c841ff153d5
developdaly <developdaly@9eefaf49-d13b-4547-b2ae-f4dffa355a4c>
2012-11-21 02:12:08 +0000

    -n Merged maintenance to trunk
    git-svn-id: https://developdaly.svn.beanstalkapp.com/create-denton/trunk@167 9eefaf49-d13b-4547-b2ae-f4dffa355a4c

--- header.php
+++ header.php
@@ -54,53 +54,60 @@ $(document).ready(function() {
 				<h1 class="brand"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">Create <strong>Denton</strong></a></h1>
 				<div class="skip-link assistive-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a></div>
 				<div class="menu-primary-container">
-					<ul id="menu-primary" class="nav">
-						<li id="filter" class="dropdown">
-	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Filter <b class="caret"></b></a>
-                        	<ul class="option-set dropdown-menu" data-option-key="filter">
-						        <li data-filter=".photographer">	<a href="#filter">Photographer</a></li>
-						        <li data-filter=".programmer">		<a href="#filter">Programmer</a></li>
-						        <li data-filter=".film-video">		<a href="#filter">Film/Video</a></li>
-						        <li data-filter=".writer">			<a href="#filter">Writer</a></li>
-						        <li data-filter=".web-designer">	<a href="#filter">Web Designer</a></li>
-						        <li data-filter=".graphic-designer"><a href="#filter">Graphic Designer</a></li>
-						        <li data-filter=".other">			<a href="#filter">Other</a></li>
-						        <li data-filter="*" class="selected"><a href="#filter">Reset</a></li>
-	                        </ul>
-						</li>
-					</ul>
 					
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target="#menu-secondary">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</a>
- 
-					<ul class="nav nav-collapse collapse pull-right">
-
-						<li class="menu-item highlight"><a href="#" data-reveal-id="feedback" data-animation="fade" data-animationSpeed="12000">Feedback</a></li>
-
-						<li class="menu-item"><a href="#" data-reveal-id="about" data-animation="fade" data-animationSpeed="12000">About</a></li>
-
-						<?php if( is_user_logged_in() ): ?>
 					
-						<li>
-							<a href="#" data-reveal-id="edit-profile" data-animation="fade" data-animationSpeed="12000">Edit profile</a>
+					<ul id="menu-secondary" class="nav nav-collapse collapse pull-right">
+
+						<li class="dropdown">
+
+							<?php
+							if( is_user_logged_in() ):
+							global $current_user;
+							get_currentuserinfo();
+							?>
+
+							<a href="#" class="menu-item dropdown-toggle" data-toggle="dropdown">
+								<img src="<?php echo cd_get_avatar( $current_user->ID ); ?>" class="avatar pull-left" height="25" width="25" alt="<?php echo get_user_meta( $current_user->ID, 'first_name', true ); ?> <?php echo get_user_meta( $current_user->ID, 'last_name', true ); ?>">
+								<?php echo $current_user->user_login; ?> <b class="caret"></b></a>
+
+							<ul class="dropdown-menu">
+								<li class="menu-item">			<a href="#" data-animation="fade" data-animationSpeed="12000" data-reveal-id="edit-profile">Edit profile</a></li>
+								<li class="menu-item">			<a href="<?php echo wp_logout_url( get_home_url() ); ?>" title="Logout">Logout</a></li>
+								<li class="divider"></li>
+								<li class="menu-item">			<a href="#" data-animation="fade" data-animationSpeed="12000" data-reveal-id="about">About</a></li>
+								<li class="menu-item">			<a href="https://www.facebook.com/CreateDenton" target="_blank">Like us on Facebook</a></li>
+							</ul>
+
+							<?php else: ?>
+
+							<li class="menu-item">			<a href="#" data-animation="fade" data-animationSpeed="12000" data-reveal-id="about">About</a></li>
+							<li class="menu-item">			<a href="#" data-animation="fade" data-animationSpeed="12000" data-reveal-id="login">Login</a></li>
+							<li class="menu-item">			<a href="#" data-animation="fade" data-animationSpeed="12000" data-reveal-id="sign-up">Sign Up</a></li>
+
+							<?php endif; ?>
+
 						</li>
-						<li>
-							<a href="<?php echo wp_logout_url( get_home_url() ); ?>" title="Logout">Logout</a>
+
+					</ul>
+					
+					<ul id="menu-primary" class="nav">
+						<li id="filter">
+                        	<ul id="filters" class="option-set" data-option-key="filter">
+						        <li data-filter=".graphic-designer"><a href="#filter">Graphic Designer</a></li>
+						        <li data-filter=".web-designer">	<a href="#filter">Web Designer</a></li>
+						        <li data-filter=".photographer">	<a href="#filter">Photographer</a></li>
+						        <li data-filter=".programmer">		<a href="#filter">Programmer</a></li>
+						        <li data-filter=".film-video">		<a href="#filter">Film/Video</a></li>
+						        <li data-filter=".writer">			<a href="#filter">Writer</a></li>
+						        <li data-filter=".other">			<a href="#filter">Other</a></li>
+						        <li data-filter="*" class="selected"><a href="#filter">Reset</a></li>
+	                        </ul>
 						</li>
-						
-						<?php else: ?>
-					
-							<li>
-								<a href="#" data-reveal-id="login" data-animation="fade" data-animationSpeed="12000">Login</a>
-							</li>
-							<li>
-								<a href="#" data-reveal-id="sign-up" data-animation="fade" data-animationSpeed="12000">Sign Up</a>
-							</li>
-					
-						<?php endif; ?>
 					</ul>
 				</div>
 			</div>
--- modals.php
+++ modals.php
@@ -181,30 +181,4 @@ global $current_user;
 	
 </div><!-- .modal -->
 
-<div class="reveal-modal" id="feedback" >
-	
-	<div class="modal-header">
-		
-		<a class="close-reveal-modal">×</a>
-		
-		<h3>Feedback</h3>
-		
-	</div>
-	
-	<div class="modal-body">
-	
-		<div class="row-fluid">
-			
-			<div class="span12">
-	
-			<?php echo do_shortcode('[gravityform name="Feedback" title=false description=true ajax=true]'); ?>			
-				
-			</div>
-			
-		</div><!-- .row-fluid -->
-		
-	</div>
-	
-</div><!-- .modal -->
-
 <?php get_template_part( 'card-back' ); // Loads card-back.php ?>
