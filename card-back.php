ad85a432035e2b3a441cf54caf4da698e0621f14
Patrick Daly <patrick@developdaly.com>
2014-05-28 21:18:37 -0500

    Whitespace cleanup

--- card-back.php
+++ card-back.php
@@ -1,12 +1,10 @@
 <div id="user-{{ID}}" class="modal hide fade person card-back {{type}}" role="dialog" aria-labelledby="modal-person-label" aria-hidden="true" data-type="{{type}}">
-				
-	<figure id="vcard-lastfirst-{{ID}}" itemscope="itemscope" itemtype="http://www.data-vocabulary.org/Person/">
 
+	<figure id="vcard-lastfirst-{{ID}}" itemscope="itemscope" itemtype="http://www.data-vocabulary.org/Person/">
 		<figcaption>
 
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				
 				<header class="n" title="Name">
 					<h3 class="fn" itemprop="name">
 						<span class="given-name">{{first_name}}</span>
@@ -17,22 +15,19 @@
 			</div><!-- .modal-header -->
 
 			<div class="modal-body">
-				
 				<img src="{{{avatar}}}" height="165" width="165" alt="{{first_name}} {{last_name}}" class="thumbnail avatar pull-right">
-
 				<section class="note">
 					<header>
 						<h4>Contact</h4>
 					</header>
 					<ul class="border-left">
-						
 						{{#if website}}
 							<li class="website"><a href="{{website}}" target="_blank" class="url" itemprop="url" rel="me self external">{{website}}</a></li>
 						{{/if}}
 						{{#if twitter}}
 							<li class="twitter"><a href="http://twitter.com/{{twitter}}" target="_blank"><i class="icon-twitter"></i> {{twitter}}</a></li>
 						{{/if}}
-						
+
 						<?php if( is_user_logged_in() ) { ?>
 							{{#if phone}}
 								<li class="tel"><abbr class="value" itemprop="tel" title="+1{{phone}}">{{phone}}</abbr></li>
@@ -46,7 +41,6 @@
 							{{/if}}
 							<li><small><a href="#" data-reveal-id="login" data-animation="fade" data-animationSpeed="12000">Login to email this user</a></small></li>
 						<?php } ?>
-						
 					</ul>
 				</section>
 
@@ -57,7 +51,7 @@
 					</header>
 					<p>{{description}}</p>
 					{{/if}}
-					
+
 					{{#if skills}}
 					<section>
 						<header><h4>Skills</h4></header>
@@ -68,15 +62,11 @@
 						</ul>
 					</section>
 					{{/if}}
-
 				</section>
-
 			</div><!-- modal-body -->
 
 		</figcaption><!-- .figcaption -->
-
 	</figure>
-
 </div><!-- .vcard -->
 
 <div id="contact-{{ID}}" class="modal hide fade {{type}}">
