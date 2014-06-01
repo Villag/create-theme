a359d3e2811dcdf6c909f73731d5fd8c681dcd15
Patrick Daly <patrick@developdaly.com>
2014-06-01 07:29:15 -0500

    Much improved modals

--- functions.php
+++ functions.php
@@ -120,6 +120,7 @@ function create_scripts() {
 	wp_enqueue_script( 'bootstrap',						get_stylesheet_directory_uri() .'/assets/js/bootstrap.min.js',			array( 'jquery' ), '1.1', true );
 	wp_enqueue_script( 'isotope',						get_stylesheet_directory_uri() .'/assets/js/jquery.isotope.min.js',		array( 'jquery' ), '1.1', true );
 	wp_enqueue_script( 'bootstrap-modalmanager', 		get_stylesheet_directory_uri() .'/assets/js/bootstrap-modalmanager.js',	array( 'jquery' ), '1.1', true );
+	wp_enqueue_script( 'bootstrap-modal', 				get_stylesheet_directory_uri() .'/assets/js/bootstrap-modal.js',		array( 'jquery' ), '1.1', true );
 	wp_enqueue_script( 'iealert',						get_stylesheet_directory_uri() .'/assets/js/iealert/iealert.min.js',	array( 'jquery' ), '1.1', true );
 	wp_enqueue_script( 'create-skip-link-focus-fix',	get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js',		array( 'jquery' ), '1.1', true );
 	wp_enqueue_script( 'app',							get_stylesheet_directory_uri() .'/assets/js/app.js',					array( 'jquery' ), '1.1', true );
